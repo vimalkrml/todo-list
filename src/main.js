adc4c0dce856f306669d592b327c3143a1985db0
Vimal KR <vimalkr141@gmail.com>
2022-06-07 15:50:19 +0530

    first commit

---
+++ src/main.js
@@ -0,0 +1,26 @@
+var push = document.querySelector('#push');
+var newInput = document.querySelector('#newtask input');
+var tasks = document.querySelector('#tasks');
+push.onclick = function () {
+    if (newInput.value.length == 0) {
+        alert("No Values Entered ! Please Enter")
+    }
+    else {
+        tasks.innerHTML += `
+            <div class="task">
+                <span id="taskname">
+                    ${newInput.value}
+                </span>
+                <button class="delete">
+                    <span>Delete</span>
+                </button>
+            </div>
+        `;
+        var current_tasks = document.querySelectorAll(".delete");
+        for (var i = 0; i < current_tasks.length; i++) {
+            current_tasks[i].onclick = function () {
+                this.parentNode.remove();
+            }
+        }
+    }
+}
