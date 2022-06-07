4aa5322da55f74a08e12cf3014a62b6d2a5c6af4
Vimal KR <vimalkr141@gmail.com>
2022-06-07 17:17:12 +0530

    small changes in js

--- src/main.js
+++ src/main.js
@@ -25,19 +25,3 @@ push.onclick = function () {
         }
     }
 }
-function myFunction() {
-    var input, filter, ul, li, a, i, textValue;
-    input = document.getElementById("myInput");
-    filter = input.value;
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        textValue = a.textContent || a.innerText;
-        if (textValue.indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-        }
-    }
-}
